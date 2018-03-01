cc84cd313bad1e6d942b287376af99a76562a7ce
Sandhya Rao <smudra@yahoo.com>
2018-02-28 23:16:08 -0600

    added color but not working in DOM

--- converter.js
+++ converter.js
@@ -1,11 +1,95 @@
 
 // to clear temp input field
 function clearTemp() {
-    document.getElementById("input-field").value = "";
+    document.getElementById("input-temp").value = "";
 }
 
-const keyEnter = document.getElementById("input-field").addEventListener("keypress", function(event) {
-    if (event.key == "enter") {
-        document.getElementById("input-field").innerHTML = "";
-    }
-});
+
+// use function to convert temperature
+ var input = document.getElementById("converter");           input.addEventListener("click", determineConverter);
+
+
+ // Radio buttons
+ var celcius = document.getElementById("celcius");
+ var farenheit = document.getElementById("farenheit");
+
+
+ // output Converted Temperature
+ var outputTemp = document.getElementById("converted-temp");
+ var convertedTemp = 0;
+
+
+    // convert to celcius
+    function toCelsius(C) {
+        C = parseFloat(C);
+        return ((C - 32) / 1.8);
+        //document.getElementById("converted-temp").innerHTML = ((C - 32) / 1.8);
+    };
+
+    // convert to farenheit
+    function toFarenheit(F) {
+        F = parseFloat(F);
+        return ((F * 1.8) + 32);
+        //document.getElementById("converted-temp").innerHTML = ((F * 1.8) + 32);
+    };
+
+
+    function determineConverter (clickEvent) {
+        var tempInput = document.getElementById("input-temp").value;
+        
+
+        if (celcius.checked) {
+            // console.log("Checking C: ", tempInput);
+            convertedTemp = toCelsius(Number(tempInput));
+            // console.log("Checking converted F: ", convertedTemp);
+
+            if (convertedTemp < 0) {
+                outputTemp.style.color = "blue";
+
+            } else if (convertedTemp >= 0 && convertedTemp < 33) {
+                outputTemp.style.color = "green";
+
+            } else if (convertedTemp > 33) {
+                outputTemp.style.color = "red";
+            };
+
+            let tempColor = convertedTemp.toFixed(2) + " degree Celsius";
+            
+            outputTemp = tempColor;
+
+            console.log("what is outputTemp.innerHTML showing?", outputTemp);
+
+        } // converting to Fahrenheit
+        else if (farenheit.checked) {
+            console.log("Checking F: ", tempInput);
+            convertedTemp = toFarenheit(Number(tempInput));
+            console.log("Checking converted F: ", convertedTemp);
+
+            if (convertedTemp < 32) {
+                outputTemp.style.color = "blue";
+
+            } else if (convertedTemp >= 32 && convertedTemp < 91) {
+                outputTemp.style.color = "green";
+
+            } else if (convertedTemp > 90) {
+                outputTemp.style.color = "red";
+            };
+
+            let tempColor = convertedTemp.toFixed(2) + " degree Fahrenheit";
+            
+            outputTemp = tempColor;
+
+            console.log("what is outputTemp.innerHTML showing?", outputTemp);
+
+        };
+        //document.getElementById("converted-temp").innerHTML = convertedTemp + " degrees Fahren";
+   };
+   console.log("convert to F", convertedTemp);
+
+//console.log(determineConverter("outputTemp", outputTemp));
+   // To Create colors for temperature
+//    if (outputTemp )
+
+//    if (outputTemp.innerHTML >= 32 && outputTemp.innerHTML <= 90) {
+//         outputTemp.style.color = "red";
+//    }
---
+++ newConverter.js
@@ -0,0 +1,16 @@
+
+
+    //let inputTemp = document.getElementById("converter");
+    let tTemp = 65;
+
+    console.log(tTemp);
+   // document.addEventListener("keypress", function(event) {
+            //if (event.key === "Enter") {
+
+            var celCalculation = ((tTemp - 32) / 1.8) + " Degree Celcius";
+                console.log("conversion: ", celCalculation);
+
+
+   // document.getElementById("converted-temp").innerHTML = celCalculation;
+            //}
+       // });
